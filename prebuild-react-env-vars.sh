86270f911571354fc7beeddccd18221030305bd7
Antonio Nieto García <antonio.nieto.garcia@externos.gloval.es>
2024-02-08 16:47:45 +0100

    documetnación script

--- prebuild-react-env-vars.sh
+++ prebuild-react-env-vars.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Este código busca configurar las variables de entorno para una aplicación React antes de construir la imagen de Docker.
+
+# Primero, busca en los archivos JavaScript y JSX en el directorio src/ referencias a variables de entorno como process.env, 
+# window._env_ o import.meta.env. Si encuentra alguna, significa que la aplicación está utilizando variables de entorno.
+
+# Luego, si se encontraron referencias a variables de entorno, hace dos cosas:
+# 1) Reemplaza todas las referencias a process.env e import.meta.env por window._env_. Esto hace que en lugar de leer las 
+#    variables de entorno del sistema, las lea desde el archivo react.env.js que será generado después.
+# 2) Busca los archivos index.html e index.htm, y antes de cerrar la etiqueta </head> agrega una línea para importar el archivo react.env.js 
+#    generado previamente. Esto hace disponibles las variables de entorno para el código JavaScript a través de window._env_.
+
+# De esta forma, configura la aplicación React para que en lugar de utilizar las variables de entorno del sistema (que no puede directamente), 
+# las tome desde el archivo react.env.js que será generado con los valores deseados para cada entorno (desarrollo, producción, etc).
+
+# Esto permite montar un contenedor de Docker con las variables de entorno configuradas, 
+# para que la aplicación se ejecute correctamente en cada entorno.
+
 # Buscar en archivos js/jsx en src/
 found=false
 for file in $(find src/ -name '*.js' -o -name '*.jsx'); do
